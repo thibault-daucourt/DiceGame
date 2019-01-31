40f38260d428577f8db4697dc03226b452871480
sagarion <thibault.daucourt@gmail.com>
2019-01-31 15:51:45 +0100

    Ajoutez des fichiers projet.

---
+++ DiceGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
